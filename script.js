28f7da50e6767cd3dc2bc73f3893d047d2264a37
Mehta Sudesh <Mehta@iazi.ch>
2017-05-28 07:27:03 +0200

    Session 8 Assignment 3

---
+++ script.js
@@ -0,0 +1,12 @@
+
+/*hide input elements using JQUery */
+function hideElements()
+{
+    $("input").hide();
+};
+
+/*show input elements using JQUery */
+function showElements()
+{
+    $("input").show();
+};
